caee824234999edea19662b869f53bdf0f4a2bb6
Big-Smile-5 <big.smile318@gmail.com>
2023-06-13 05:32:14 +0300

    backend: init

---
+++ backend/db/models/Order.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const OrderSchema = new Schema({
+    userId: {
+        type: String,
+    },
+    items: [{
+        productId: {
+            type: String,
+        },
+        name: String,
+        quantity: {
+            type: Number,
+            required: true,
+            min: [1, 'Quantity can bot be zero.'],
+        },
+        price: Number,
+    }],
+    bill: {
+        type: Number,
+        required: true,
+    },
+    date_added: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+module.exports = Order = mongoose.model('order', OrderSchema)
